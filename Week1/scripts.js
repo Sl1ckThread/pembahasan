47e05d5d571760d1b408339fdab454858a1db3b9
Sl1ckThread <mrnaufal360@gmail.com>
2024-11-04 09:24:03 +0700

    Added idk

--- Week1/scripts.js
+++ Week1/scripts.js
@@ -2,17 +2,21 @@ const getElement = (identifier = "") => document.querySelector(identifier);
 
 function addTask() {
   let nama = getElement("#nama").value;
+  let comment = getElement("#Comment").value;
+  
+  if (nama && comment) {
+    alert("Filled Sucessfully")
+  }
 
-  if (!nama) {
-      alert("Please enter a task name.");
-      return;
+  if (!nama && !comment) {
+    alert("Please enter a task and comment.");
+    return;
   }
 
   getElement("#kepentingan").value = "gabut";
-  getElement("#priority").value = "gabut";
+  getElement("#nama").value = "";
+  getElement("#Comment").value = "";
+  
 }
-getElement("#submit").addEventListener("click", hasil);
 
-function hasil() {
-  alert("Filled sucessfully")
-}
+getElement("#submit").addEventListener("click", addTask);
